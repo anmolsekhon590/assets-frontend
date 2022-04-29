6c8458359f7bbe1c35ba412cb4b1b348a23f7c3f
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-04-28 20:41:33 -0400

    added redirects

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
+import { Navigate } from "react-router-dom";
 import { User } from "./User";
 export default function Login() {
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const { register, handleSubmit } = useForm();
 
     // to store login message
     const [loginMessage, setLoginMessage] = useState(null);
@@ -45,24 +46,34 @@ export default function Login() {
         });
     }
 
-    return (
-        <div className="max-w-xl m-auto bg-white mt-10">
-            <form className="shadow-md rounded-md p-8 text-2xl" onSubmit={handleSubmit(onSubmit)} >
-                <div className="mb-3">
-                    <label htmlFor="username">Username: </label>
-                    <input className="w-full border-2 p-2" type="text" name="username" {...register("username")} placeholder="Enter username here" />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password">Password: </label>
-                    <input className="w-full border-2 p-2" type="password" {...register("password")} placeholder="Enter Password Here" />
-                </div>
+    if (sessionStorage.getItem("jwtToken") !== null) {
+        return (
+            <div>
+                {<Navigate to="/assets" />}
+            </div>
+        )
+    } else {
 
-                <div className={loginMessageColor}>
-                    {loginMessage}
-                </div>
+        return (
+            <div className="max-w-xl m-auto bg-white mt-10 rounded-md">
+                <form className="shadow-md rounded-md p-8 text-2xl" onSubmit={handleSubmit(onSubmit)} >
+                    <div className="mb-3">
+                        <label htmlFor="username">Username: </label>
+                        <input className="w-full border-2 p-2 rounded-md" type="text" name="username" {...register("username")} placeholder="Enter username here" />
+                    </div>
+                    <div className="mb-3">
+                        <label htmlFor="password">Password: </label>
+                        <input className="w-full border-2 p-2 rounded-md" type="password" {...register("password")} placeholder="Enter Password Here" />
+                    </div>
 
-                <button className="w-full bg-color-2 text-color-4 mb-3 rounded-md py-2" type="submit">Login</button>
-            </form>
-        </div>
-    )
+                    <div className={loginMessageColor}>
+                        {loginMessage}
+                    </div>
+
+                    <button className="w-full bg-color-2 text-color-4 mb-3 rounded-md py-2" type="submit">Login</button>
+                </form>
+            </div>
+        )
+    }
+
 }
