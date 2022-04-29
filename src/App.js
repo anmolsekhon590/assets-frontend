72820f0c55cf6f0bfdaf03f1d53ef82c13db3238
anmolsekhon590 <anmolsekhon590@gmail.com>
2022-04-28 20:39:42 -0400

    added react router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import Asset from './Components/Asset/Asset';
 import Assets from './Components/Assets/Assets';
 import Header from './Components/Header/Header';
 import Login from './Components/Login/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
+
     <div className="App">
       <Header />
-      {/* <Login /> */}
-      <Assets />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/assets" element={<Assets />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
